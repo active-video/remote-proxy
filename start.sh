5053e1296a564684028cfaf446f605f9e772b6cb
Chad Wagner <cwagner@activevideo.com>
2015-01-01 19:51:24 -0800

    Added license and link to repo

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env node
+#!/bin/bash
 
+#Make sure this is executable, or chmod 777
 
 cd /var/www/html/cloudtv-remote-proxy/node_modules/cloudtv-remote-proxy;
-nohup node server.js -p 9083 &
+nohup node index.js -p 9083 &
