f1330165a67b750f7779b482954940d8ad5e91b3
cwagner <cwagner@activevideo.com>
2017-11-28 09:18:51 -0800

    - merge back in npm changes

--- public/client.js
+++ public/client.js
@@ -6,30 +6,23 @@
  * remember, if you need more than 1 script, just
  * separate them by spaces
  */
-
 (function(){
-
     if(location.href === 'about:blank'){
         return;
     }
-
     //Not supported on i-frames
     if(window.location !== window.parent.location){
         return;
     }
-
     var host = '%HOST%',
         PORT = '%PORT%',
         isActive = false;
-
     //global
     __socket = null;
     s = null;//script tag
-
     //lifecycle functions
     var reopenSocket = function () {
         isActive = true;
-
         //restart if we have a socket and it's not in a connected state
         if(__socket !== null && __socket.disconnected && __socket.io.readyState !== 'opening'){
             //console.log('SOCKET - Reconnecting ' + location.href);
@@ -39,11 +32,9 @@
             startSocket();
         }
     }
-
     var closeSocket = function (evt) {
         isActive = false;
         s.onload = undefined;
-
         if(__socket){
             //console.log('SOCKET - Disconnecting ' + location.href);
             if(evt.type === 'close'){
@@ -51,44 +42,33 @@
             }else{
                 __socket.disconnect();
             }
-
         }
     }
-
     //Keep Track of focus/blur so that we don't respond when we aren't the active window
     window.addEventListener('focus', reopenSocket);
     window.addEventListener('blur', closeSocket);
     window.addEventListener('close', closeSocket);
-
     var simulateKeyPress = function(keyCode, el){
         var eventObj = document.createEventObject ?
             document.createEventObject() : document.createEvent('Events');
-
         if(eventObj.initEvent){
             eventObj.initEvent('keydown', true, true);
         }
-
         eventObj.keyCode = keyCode;
         eventObj.which = keyCode;
-
         var target = el || document.activeElement;
         target.dispatchEvent ? target.dispatchEvent(eventObj) : target.fireEvent('onkeydown', eventObj);
     };
-
     var onMessage = window.__onMessage = function(message){
         //Only act on messages if we are the top window
         //console.log('SOCKET.onMessage', isActive, message, location.href);
         if(!isActive){
             return;
         }
-
         switch(message.type){
-
             case 'key':
                 simulateKeyPress(message.keyCode);
                 break;
-
-
             default:
                 if(window.onmessage){
                     window.onmessage({data: message});
@@ -96,29 +76,21 @@
                 break;
         }
     };
-
-
-
     var loadSocketIo = function(){
         //console.log('SOCKET - loadSocketIo() ' + location.href);
-
         var CLIENTID = navigator.avClient ? navigator.avClient.id : 'shared';
         var socketScriptUrl = '%HOST%/socket.io/socket.io.js';
-
         console.log('SOCKET - load WebRemote: ' +  socketScriptUrl + ' on page ' + location.href);
-
         s = document.createElement('script');
         s.setAttribute('type','text/javascript')
         s.setAttribute('src', socketScriptUrl);
         s.onload = startSocket;
         document.getElementsByTagName('head')[0].appendChild(s);
     }
-
     var startSocket = function(){
         if(__socket){
             return;
         }
-
         if(typeof(io) === 'undefined'){
             setTimeout(startSocket, 30);
         }else{
@@ -127,12 +99,10 @@
             __socket = io('%HOST%?role=%ROLE%&clientid=' + (navigator.avClient ? navigator.avClient.id : 'shared'));
             __socket.on('message', window.__onMessage);
         }
-    }
-
+    };
     if(document.readyState === 'complete'){
         loadSocketIo();
     }else{
         document.addEventListener('DOMContentLoaded', loadSocketIo);
     }
-
-})()
+})();
